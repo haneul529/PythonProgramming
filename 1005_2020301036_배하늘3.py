92737d41ca2a0b8cbd09191bd0d6f919ca443ebe
haneul529 <lucia00529@skuniv.ac.kr>
2023-10-09 11:20:43 +0900

    Add Python File[1005_2020301036_배하늘3.py]

---
+++ 1005_2020301036_배하늘3.py
@@ -0,0 +1,65 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/turtle.py
+********** 거북이 게임을 시작합니다. **********
+거북이 선수를 선택하세요.
+거북이1번 : 거북이
+거북이2번 : 꼬북이
+거북이3번 : 자라
+거북이4번 : 별주부
+Traceback (most recent call last):
+  File "C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/turtle.py", line 1, in <module>
+    import turtle as t
+  File "C:\Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005\turtle.py", line 14, in <module>
+    t.speed(10)
+AttributeError: partially initialized module 'turtle' has no attribute 'speed' (most likely due to a circular import)
+
+= RESTART: C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/turtle_race.py
+********** 거북이 게임을 시작합니다. **********
+거북이 선수를 선택하세요.
+거북이1번 : 거북이
+거북이2번 : 꼬북이
+거북이3번 : 자라
+거북이4번 : 별주부
+Traceback (most recent call last):
+  File "C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/turtle_race.py", line 1, in <module>
+    import turtle as t
+  File "C:\Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005\turtle.py", line 14, in <module>
+    t.speed(10)
+AttributeError: partially initialized module 'turtle' has no attribute 'speed' (most likely due to a circular import)
+
+= RESTART: C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/turtle_race.py
+********** 거북이 게임을 시작합니다. **********
+거북이 선수를 선택하세요.
+거북이1번 : 거북이
+거북이2번 : 꼬북이
+거북이3번 : 별주부
+거북이4번 : 자라
+Traceback (most recent call last):
+  File "C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/turtle_race.py", line 1, in <module>
+    import turtle
+  File "C:\Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005\turtle.py", line 14, in <module>
+    t.speed(10)
+AttributeError: partially initialized module 'turtle' has no attribute 'speed' (most likely due to a circular import)
+>>> 
+= RESTART: C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/turtle_race.py
+********** 거북이 게임을 시작합니다. **********
+거북이 선수를 선택하세요.
+거북이1번 : 거북이
+거북이2번 : 꼬북이
+거북이3번 : 자라
+거북이4번 : 별주부
+Traceback (most recent call last):
+  File "C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/turtle_race.py", line 106, in <module>
+    maxValue = score_z[i]
+NameError: name 'score_z' is not defined. Did you mean: 'score_x'?
+>>> 
+= RESTART: C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/turtle_race.py
+********** 거북이 게임을 시작합니다. **********
+거북이 선수를 선택하세요.
+거북이1번 : 거북이
+거북이2번 : 꼬북이
+거북이3번 : 자라
+거북이4번 : 별주부
+거북이님, 1등입니다! 축하합니다!!
