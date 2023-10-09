9abedddee42bb5ab7f58e6c09066da81391710c9
haneul529 <lucia00529@skuniv.ac.kr>
2023-10-09 11:12:12 +0900

    Add Python File[1005_2020301036_배하늘.py]

---
+++ 1005_2020301036_배하늘.py
@@ -0,0 +1,42 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/Code07-02.py
+1번째 숫자 : 10
+2번째 숫자 : 20
+3번째 숫자 : 30
+4번째 숫자 : 40
+합계 ==> 100
+>>> aa=[]
+>>> aa.append(0)
+>>> aa.append(0)
+>>> aa.append(0)
+>>> aa.append(0)
+>>> print(aa)
+[0, 0, 0, 0]
+>>> aa=[]
+>>> for i in range(0. 100) :
+...     
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> aa=[]
+>>> for i in range(0, 100) :
+...     aa.append(0)
+... len(aa)
+SyntaxError: invalid syntax
+>>> len(aa)
+0
+>>> aa=[]
+>>> for i in range(0, 100) :
+...     aa.append(0)
+... len(aa)
+SyntaxError: invalid syntax
+>>> 
+= RESTART: C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/append.py
+>>> 
+= RESTART: C:/Users/lucia/OneDrive/바탕 화면/하늘/2023/2학기/전공/오픈소스프로그래밍/1005/append.py
+>>> aa=[]
+>>> for i in range(0, 100) :
+...     aa.append(0)
+... 
+>>> len(aa)
+100
